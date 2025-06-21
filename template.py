420ae15242d64f7223662c18ccc872e75670e3ec
PHANUWAT\panuw <Panuwat_nine@hotmail.com>
2025-06-22 01:33:01 +0700

    format code

--- template.py
+++ template.py
@@ -2,7 +2,9 @@ import os
 from pathlib import Path
 import logging
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 proect_name = "data_science_project"
 
@@ -31,16 +33,18 @@ list_of_files = [
 
 for file_path in list_of_files:
     file_path = Path(file_path)
-    file_dir,file_name = os.path.split(file_path)
+    file_dir, file_name = os.path.split(file_path)
 
     if file_dir != "":
         os.makedirs(file_dir, exist_ok=True)
         logging.info(f"Creating directory: {file_dir} for file: {file_name}")
 
     if (not os.path.exists(file_path)) or (os.path.getsize(file_path) == 0):
-        with open(file_path, 'w') as f:
+        with open(file_path, "w") as f:
             pass
             logging.info(f"Creating file: {file_path}")
 
     else:
-        logging.info(f"File already exists: {file_path} and is not empty, skipping creation.")
+        logging.info(
+            f"File already exists: {file_path} and is not empty, skipping creation."
+        )
